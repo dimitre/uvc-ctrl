459a2f2d7509ddbedf24cb9161545c84a840fb47
Dimitre <dimitre@users.noreply.github.com>
2025-10-02 05:15:03 -0300

    runner

--- uvc-ctrl.cpp
+++ uvc-ctrl.cpp
@@ -1,35 +1,54 @@
-// cl /EHsc /std:c++17 uvc_ctrl.cpp  mfplat.lib mf.lib mfreadwrite.lib mfuuid.lib shlwapi.lib
+// cl /EHsc /std:c++17 uvc_ctrl.cpp  mfplat.lib mf.lib mfreadwrite.lib strmiids.lib shlwapi.lib ole32.lib oleaut32.lib
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
 #include <windows.h>
 #include <mfapi.h>
 #include <mfidl.h>
 #include <mfreadwrite.h>
-#include <dshow.h>        // IAMVideoProcAmp, IAMCameraControl
+#include <dshow.h>
+#include <shlwapi.h>
+#include <cstdio>
 
 #pragma comment(lib, "mfplat.lib")
 #pragma comment(lib, "mf.lib")
 #pragma comment(lib, "mfreadwrite.lib")
-#pragma comment(lib, "mfuuid.lib")
+#pragma comment(lib, "strmiids.lib")
 #pragma comment(lib, "shlwapi.lib")
+#pragma comment(lib, "ole32.lib")
+#pragma comment(lib, "oleaut32.lib")
 
-template <class T> void SafeRelease(T** pp) {
-    if (*pp) { (*pp)->Release(); *pp = nullptr; }
+template <class T> void SafeRelease(T** pp) { if (*pp) { (*pp)->Release(); *pp = nullptr; } }
+
+void PrintPropRange(IAMVideoProcAmp* vpa, VideoProcAmpProperty id, const char* name)
+{
+    long mn, mx, step, def, caps;
+    if (SUCCEEDED(vpa->GetRange(id, &mn, &mx, &step, &def, &caps)))
+        printf("%-20s  min=%4ld  max=%4ld  step=%3ld  default=%4ld  flags=%#lx\n",
+               name, mn, mx, step, def, caps);
 }
 
-int main() {
+void PrintCamRange(IAMCameraControl* cam, CameraControlProperty id, const char* name)
+{
+    long mn, mx, step, def, caps;
+    if (SUCCEEDED(cam->GetRange(id, &mn, &mx, &step, &def, &caps)))
+        printf("%-20s  min=%4ld  max=%4ld  step=%3ld  default=%4ld  flags=%#lx\n",
+               name, mn, mx, step, def, caps);
+}
+
+int main()
+{
     CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
     MFStartup(MF_VERSION);
 
-    IMFMediaSource* source = nullptr;
-    IMFAttributes*  attrs  = nullptr;
-    IMFActivate**   acts   = nullptr;
-    UINT32          count  = 0;
-
+    IMFAttributes* attrs = nullptr;
+    IMFActivate** acts   = nullptr;
+    UINT32         count = 0;
     MFCreateAttributes(&attrs, 1);
-    attrs->SetGUID(MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE,
-                   MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE_VIDCAP_GUID);
+    attrs->SetGUID(MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE, MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE_VIDCAP_GUID);
     MFEnumDeviceSources(attrs, &acts, &count);
+    if (count == 0) { puts("No camera"); return 0; }
 
-    if (count == 0) return 0;
+    IMFMediaSource* source = nullptr;
     acts[0]->ActivateObject(__uuidof(IMFMediaSource), (void**)&source);
 
     IAMVideoProcAmp*  vpa = nullptr;
@@ -37,20 +56,34 @@ int main() {
     source->QueryInterface(&vpa);
     source->QueryInterface(&cam);
 
-    /* ----------  example: set brightness to 150 (0-255)  ---------- */
-    long val, flags;
-    vpa->GetRange(VideoProcAmp_Brightness, &min, &max, &step, &def, &caps);
-    vpa->Set(VideoProcAmp_Brightness, 150, VideoProcAmp_Flags_Manual);
+    puts("\n=== VideoProcAmp (brightness, contrast, etc.) ===");
+    if (vpa) {
+        PrintPropRange(vpa, VideoProcAmp_Brightness, "Brightness");
+        PrintPropRange(vpa, VideoProcAmp_Contrast,   "Contrast");
+        PrintPropRange(vpa, VideoProcAmp_Hue,        "Hue");
+        PrintPropRange(vpa, VideoProcAmp_Saturation, "Saturation");
+        PrintPropRange(vpa, VideoProcAmp_Sharpness,  "Sharpness");
+        PrintPropRange(vpa, VideoProcAmp_Gamma,      "Gamma");
+        PrintPropRange(vpa, VideoProcAmp_Gain,       "Gain");
+        PrintPropRange(vpa, VideoProcAmp_WhiteBalance, "WhiteBalance");
 
-    /* ----------  example: fix exposure to -6 (logarithmic)  ------- */
-    if (cam) {
-        cam->GetRange(CameraControl_Exposure, &min, &max, &step, &def, &caps);
-        cam->Set(CameraControl_Exposure, -6, CameraControl_Flags_Manual);
+        // example: set brightness to mid-range
+        long mn, mx, step, def, caps;
+        vpa->GetRange(VideoProcAmp_Brightness, &mn, &mx, &step, &def, &caps);
+        long mid = (mn + mx) / 2;
+        vpa->Set(VideoProcAmp_Brightness, mid, VideoProcAmp_Flags_Manual);
+        printf("\nSet brightness → %ld\n", mid);
     }
 
-    /* ----------  read back ----------------------------------------- */
-    vpa->Get(VideoProcAmp_Brightness, &val, &flags);
-    printf("brightness = %ld\n", val);
+    puts("\n=== CameraControl (exposure, focus, zoom, etc.) ===");
+    if (cam) {
+        PrintCamRange(cam, CameraControl_Exposure, "Exposure");
+        PrintCamRange(cam, CameraControl_Focus,    "Focus");
+        PrintCamRange(cam, CameraControl_Zoom,     "Zoom");
+        PrintCamRange(cam, CameraControl_Pan,      "Pan");
+        PrintCamRange(cam, CameraControl_Tilt,     "Tilt");
+        PrintCamRange(cam, CameraControl_Roll,     "Roll");
+    }
 
     SafeRelease(&vpa);
     SafeRelease(&cam);
@@ -58,7 +91,6 @@ int main() {
     for (UINT32 i = 0; i < count; ++i) acts[i]->Release();
     CoTaskMemFree(acts);
     SafeRelease(&attrs);
-
     MFShutdown();
     CoUninitialize();
     return 0;
